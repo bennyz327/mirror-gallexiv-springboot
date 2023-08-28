ab2e1022f05fd44c2af180673306ed4f30a58824
lleon <maxoryza6330@gmail.com>
2023-08-28 10:47:54 +0800

    基礎CRUD完成

--- src/main/java/com/team/gallexiv/model/Comment.java
+++ src/main/java/com/team/gallexiv/model/Comment.java
@@ -2,6 +2,8 @@ package com.team.gallexiv.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +11,14 @@ import lombok.Setter;
 import java.sql.Timestamp;
 import java.util.Collection;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Setter
 @Getter
 @Entity
 @Table(name = "comment", schema = "gallexiv")
 public class Comment {
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "commentId")
@@ -24,22 +29,33 @@ public class Comment {
     private String commentText;
 
     @Basic
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss:SSS")
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "commentTime")
     private Timestamp commentTime;
+    // private Instant commentTime; // for lleon's own memo
+
+    // 寫入DB前先建立時間
+    @PrePersist
+    public void onCommentCreate() {
+        if (commentTime == null) {
+            commentTime = new Timestamp(System.currentTimeMillis());
+        }
+    }
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "postId", referencedColumnName = "postId", nullable = false)
-    @JsonIncludeProperties({"postId"})
+    @JsonIncludeProperties({ "postId" })
     private Post postByPostId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", referencedColumnName = "userId", nullable = false)
-    @JsonIncludeProperties({"userId","userName"})
+    @JsonIncludeProperties({ "userId", "userName" })
     private Userinfo userinfoByUserId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parentCommentId", referencedColumnName = "commentId")
-    @JsonIncludeProperties({"parentCommentId"})
+    @JsonIncludeProperties({ "userId", "parentCommentId", "commentText" })
     private Comment commentByParentCommentId;
 
     @OneToMany(mappedBy = "commentByParentCommentId")
@@ -47,7 +63,7 @@ public class Comment {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_status", referencedColumnName = "code_id")
-    @JsonIncludeProperties({"statusId","statusType","statusCategory","statusName"})
+    @JsonIncludeProperties({ "statusId", "statusName" })
     private Status commentStatusByStatusId;
 
 //    @Override
