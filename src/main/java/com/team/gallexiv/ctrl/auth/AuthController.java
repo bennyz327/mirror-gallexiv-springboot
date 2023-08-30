84403bf3eb17ae45efe4e8c36a132632527f7446
benny <bennyz327@gmail.com>
2023-08-30 17:12:45 +0800

    驗證WIP

--- src/main/java/com/team/gallexiv/ctrl/auth/AuthController.java
+++ src/main/java/com/team/gallexiv/ctrl/auth/AuthController.java
@@ -1,6 +1,10 @@
 package com.team.gallexiv.ctrl.auth;
 
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.map.MapUtil;
 import com.google.code.kaptcha.Producer;
+import com.team.gallexiv.lang.VueData;
+import com.team.gallexiv.model.CaptchaInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +14,21 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
-import java.util.UUID;
 
 @RestController
 public class AuthController extends BaseController {
 
     @Autowired
     Producer producer;
-    @GetMapping("/captcha")
-    public String captcha() throws IOException {
 
+
+    @GetMapping("/captcha")
+    public VueData captcha() throws IOException {
+
+        //產生驗證碼和圖片
         String key = UUID.randomUUID().toString();
         String code = producer.createText();
         BufferedImage image = producer.createImage(code);
-
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(image, "jpg", baos);
 
@@ -32,7 +37,15 @@ public class AuthController extends BaseController {
         String str = "data:image/jpeg;base64,";
         String base64Img = str + encoder.encodeToString(baos.toByteArray());
 
+        //儲存到MySQL
+        CaptchaInfo captchaInfo = new CaptchaInfo(key,code);
 
-        return "captcha";
+        return VueData.ok(
+                MapUtil
+                        .builder()
+                        .put("token", key)
+                        .put("base64Img", base64Img)
+                        .build()
+        );
     }
 }
