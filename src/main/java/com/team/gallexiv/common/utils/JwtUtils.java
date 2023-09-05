a810fe383ea68e9665b4914c134e2e5124c747cd
benny-desktop <bennyz327@gmail.com>
2023-09-06 03:13:54 +0800

    更改調JWT工具類棄用的方法

--- src/main/java/com/team/gallexiv/common/utils/JwtUtils.java
+++ src/main/java/com/team/gallexiv/common/utils/JwtUtils.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 import static io.jsonwebtoken.security.Keys.secretKeyFor;
@@ -17,39 +18,36 @@ import static io.jsonwebtoken.security.Keys.secretKeyFor;
 public class JwtUtils {
 
 	private long expire;
-	private String secret;
 	private String header;
+	private SecretKey secretKey;
 
 	// 生成jwt
 	public String generateToken(String username) {
 
 		Date nowDate = new Date();
 		Date expireDate = new Date(nowDate.getTime() + 1000 * expire);
+		//設定算法和密鑰
+		secretKey = secretKeyFor(SignatureAlgorithm.HS512);
 
 		return Jwts.builder()
 				.setHeaderParam("typ", "JWT")
 				.setSubject(username)
 				.setIssuedAt(nowDate)
 				.setExpiration(expireDate)// 7天過期
-				//TODO 把外面的secret環境變數整理去除，改用內部生成的secret
-//				.signWith(SignatureAlgorithm.HS512, secret)
-				.signWith(SignatureAlgorithm.HS512, secretKeyFor(SignatureAlgorithm.HS512))
+				.signWith(secretKey)
 				.compact();
 	}
 
 	// 解析jwt
 	public Claims getClaimByToken(String jwt) {
-		try {
-			return Jwts.parser()
-					.setSigningKey(secret)
-					.parseClaimsJws(jwt)
-					.getBody();
-		} catch (Exception e) {
-			return null;
-		}
+		return Jwts.parserBuilder()
+				.setSigningKey(secretKey)
+				.build()
+				.parseClaimsJws(jwt)
+				.getBody();
 	}
 
-	// jwt是否过期
+	// jwt是否過期
 	public boolean isTokenExpired(Claims claims) {
 		return claims.getExpiration().before(new Date());
 	}
