c621ab18a68cdf115415a1339f7920243cb10d48
benny-desktop <bennyz327@gmail.com>
2023-09-06 03:11:37 +0800

    清理、調整開發設定檔

--- src/main/java/com/team/gallexiv/config/WebConfiguration.java
+++ src/main/java/com/team/gallexiv/config/WebConfiguration.java
@@ -12,38 +12,35 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
-// TODO 因為靠 Security 框架設定 測試拔掉這個類之後會不會有問題
 // 跨域設定
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    //從application.properties取得設定值
-    @Value("${gallexiv.allowedOrigins}")
-    private String allowApps;
-    @Value("${gallexiv.allowedMethods}")
-    private String allowMethods;
+    //從application.properties取得設定值 TODO 測試刪除
+//    @Value("${gallexiv.allowedOrigins}")
+//    private String allowApps;
+//    @Value("${gallexiv.allowedMethods}")
+//    private String allowMethods;
 
-    //TODO 非簡單請求可能會用到的設定
-//    @Value("${gallexiv.allowedHeaders}")
-//    private String allowedHeaders;
+    // TODO 測試刪除
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        CorsRegistration reg = registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+//        reg.allowedOrigins(allowApps.split(","));
+//    }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        CorsRegistration reg = registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
-        reg.allowedOrigins(allowApps.split(","));
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        //把字串轉List設定進去
-        corsConfiguration.setAllowedOrigins(Arrays.asList(allowApps.split(",")));
-        //把方法字串轉List設定進去
-        corsConfiguration.setAllowedMethods(Arrays.asList(allowMethods.split(",")));
-        //把設定給UrlBasedCorsConfigurationSource並回傳
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
-    }
+    // TODO 測試刪除
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        //把字串轉List設定進去
+//        corsConfiguration.setAllowedOrigins(Arrays.asList(allowApps.split(",")));
+//        //把方法字串轉List設定進去
+//        corsConfiguration.setAllowedMethods(Arrays.asList(allowMethods.split(",")));
+//        //把設定給UrlBasedCorsConfigurationSource並回傳
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfiguration);
+//        return source;
+//    }
 
 }
