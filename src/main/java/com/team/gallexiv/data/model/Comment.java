95d3383077120eac66253c6ad9f2c177f18e71a5
lleon <maxoryza6330@gmail.com>
2023-09-11 10:00:34 +0800

    依照前端和DB修正select, insert, delete

--- src/main/java/com/team/gallexiv/data/model/Comment.java
+++ src/main/java/com/team/gallexiv/data/model/Comment.java
@@ -30,21 +30,13 @@ public class Comment {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "commentTime")
     private Timestamp commentTime;
-    // private Instant commentTime; // for lleon's own memo
 
-    // private Integer parentCommentId;
-
-    // 寫入DB前先建立時間 // TODO 為甚麼要加時間?
     @PrePersist
     public void onCommentCreate() {
         if (commentTime == null) {
             commentTime = new Timestamp(System.currentTimeMillis());
         }
     }
-
-    // public void setParentCommentId(Integer parentCommentId) {
-    // this.parentCommentId = parentCommentId;
-    // }
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "postId", referencedColumnName = "postId", nullable = false)
@@ -60,18 +52,6 @@ public class Comment {
     @JoinColumn(name = "parentCommentId", referencedColumnName = "commentId")
     @JsonIncludeProperties({ "userId", "parentCommentId", "commentText" })
     private Comment commentByParentCommentId;
-
-    // @Transient // 标记为 transient，不会持久化到数据库
-    // @JsonInclude(JsonInclude.Include.NON_NULL) // 只包含非空的字段
-    // private Integer parentCommentId; // 新增一个字段用于存储父评论的 commentId
-
-    // // 在构造 JSON 数据前设置 parentCommentId 的值
-    // @PostLoad
-    // public void setParentCommentId() {
-    // if (commentByParentCommentId != null) {
-    // parentCommentId = commentByParentCommentId.getCommentId();
-    // }
-    // }
 
     @OneToMany(mappedBy = "commentByParentCommentId")
     @JsonIncludeProperties({ "userId", "commentText", "parentCommentId" })
