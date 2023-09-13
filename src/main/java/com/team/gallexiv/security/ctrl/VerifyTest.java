875fb2d5a30e9a5c166195553829f06005e518a4
lleon <maxoryza6330@gmail.com>
2023-09-13 10:47:31 +0800

    為修正postController給前端使用

--- src/main/java/com/team/gallexiv/security/ctrl/VerifyTest.java
+++ src/main/java/com/team/gallexiv/security/ctrl/VerifyTest.java
@@ -1,40 +1,40 @@
-package com.team.gallexiv.security.ctrl;
+// package com.team.gallexiv.security.ctrl;
 
-import com.team.gallexiv.common.lang.VueData;
-import com.team.gallexiv.data.model.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import com.team.gallexiv.common.lang.VueData;
+// import com.team.gallexiv.data.model.UserService;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class VerifyTest extends BaseController {
+// @RestController
+// public class VerifyTest extends BaseController {
 
-    private final UserService userS;
+// private final UserService userS;
 
-    public VerifyTest(UserService userS) {
-        this.userS = userS;
-    }
+// public VerifyTest(UserService userS) {
+// this.userS = userS;
+// }
 
-    @GetMapping("/test/admin")
-    public String admin() {
-        return "admin OK";
-    }
-    @GetMapping("/test/user")
-    public String user() {
-        return "user OK";
-    }
+// @GetMapping("/test/admin")
+// public String admin() {
+// return "admin OK";
+// }
+// @GetMapping("/test/user")
+// public String user() {
+// return "user OK";
+// }
 
-    //包裝測試
-    @GetMapping("/test/userall")
-    public VueData findAllUser() {
-        return VueData.ok(userS.getAllUsers());
-    }
+// //包裝測試
+// @GetMapping("/test/userall")
+// public VueData findAllUser() {
+// return VueData.ok(userS.getAllUsers());
+// }
 
-    //redis測試
-    @GetMapping("/test/redis")
-    public VueData redisTest() {
-        redisUtil.set("redis", "这是redis的测试数据");
-        Object redis = redisUtil.get("redis");
-        return VueData.ok(redis.toString());
-    }
+// //redis測試
+// @GetMapping("/test/redis")
+// public VueData redisTest() {
+// redisUtil.set("redis", "这是redis的测试数据");
+// Object redis = redisUtil.get("redis");
+// return VueData.ok(redis.toString());
+// }
 
-}
+// }
