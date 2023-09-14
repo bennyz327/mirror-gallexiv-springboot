553ca2dd21b5b5faeb02afd362bf29d770bdd64b
lleon <maxoryza6330@gmail.com>
2023-09-14 17:51:18 +0800

    串接圖片成功

--- src/main/java/com/team/gallexiv/data/api/Pictures/PicturesController.java
+++ src/main/java/com/team/gallexiv/data/api/Pictures/PicturesController.java
@@ -1,8 +1,20 @@
 package com.team.gallexiv.data.api.Pictures;
 
+import com.team.gallexiv.data.model.PictureService;
 import com.team.gallexiv.data.model.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,13 +24,45 @@ public class PicturesController {
     @Value("${gallexiv.upload.rootpath}")
     private String rootPath;
     final UserService userS;
-    public PicturesController(UserService userS) {
+    final PictureService pictureS;
+
+    public PicturesController(UserService userS, PictureService pictureS) {
         this.userS = userS;
+        this.pictureS = pictureS;
     }
 
-//    @GetMapping(path = "/users/{id}", produces = "application/json")
-//    @Operation(description = "取得使用者 (GET BY ID)")
-//    public Userinfo showUsersOb(@PathVariable int id) {
-//        return userS.getUserById(id);
-//    }
+    // @GetMapping(path = "/users/{id}", produces = "application/json")
+    // @Operation(description = "取得使用者 (GET BY ID)")
+    // public Userinfo showUsersOb(@PathVariable int id) {
+    // return userS.getUserById(id);
+    // }
+
+    @GetMapping(value = "/test/p")
+    public ResponseEntity<List<String>> getImagesWithMediaType(@RequestParam Integer postId) throws IOException {
+        List<String> pictures = pictureS.getImgPathByPostId(postId);
+        List<String> imageUrls = new ArrayList<>();
+
+        for (String picture : pictures) {
+            try (FileInputStream fileInputStream = new FileInputStream(new File(picture))) {
+                byte[] imageBytes = new byte[fileInputStream.available()];
+                fileInputStream.read(imageBytes);
+                String base64Image = Base64.getEncoder().encodeToString(imageBytes);
+                String imageUrl = "data:image/png;base64," + base64Image;
+                imageUrls.add(imageUrl);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return ResponseEntity.ok(imageUrls);
+    }
+
+    private String getFileExtension(String filePath) {
+        int lastDotIndex = filePath.lastIndexOf('.');
+        if (lastDotIndex != -1) {
+            return filePath.substring(lastDotIndex + 1);
+        }
+        return "png"; // 如果无法从文件路径中提取扩展名，则默认为 PNG 格式
+    }
 }
---
+++ src/main/java/com/team/gallexiv/data/model/PictureService.java
@@ -0,0 +1,26 @@
+package com.team.gallexiv.data.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PictureService {
+
+    final PictureDao pictureD;
+
+    @Autowired
+    public PictureService(PictureDao pictureD) {
+        this.pictureD = pictureD;
+    }
+
+    public void savePicture(Picture picture) {
+        pictureD.save(picture);
+    }
+
+    public List<String> getImgPathByPostId(Integer postId) {
+        return pictureD.fintImgPathByPostId(postId);
+    }
+}
