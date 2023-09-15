914e5835063473a70efa887f43323e55d70b3e8d
vic0716 <vic3962449@gmail.com>
2023-09-15 15:52:13 +0800

    刪除多餘方法和注入

--- src/main/java/com/team/gallexiv/data/api/Pictures/PicturesController.java
+++ src/main/java/com/team/gallexiv/data/api/Pictures/PicturesController.java
@@ -24,51 +24,6 @@ import static com.team.gallexiv.common.lang.Const.IMG_ROOTPATH;
 @RequestMapping("/p")
 @Tag(name = "圖片控制存取")
 public class PicturesController {
-    final UserService userS;
-    final PictureService pictureS;
-
-    public PicturesController(UserService userS, PictureService pictureS) {
-        this.userS = userS;
-        this.pictureS = pictureS;
-    }
-
-    @PostMapping(path = "/upload", produces = "application/json;charset=UTF-8")
-    public String processAction(@RequestParam("myFiles") MultipartFile mf) throws IOException {
-        if (mf.isEmpty()) {
-            return "File is empty";
-        }
-
-        String fileName = mf.getOriginalFilename();
-        System.out.println("fileName:" + fileName);
-
-        // 创建上传目录（如果不存在）
-        File uploadDir = new File("C://upload");
-        if (!uploadDir.exists()) {
-            uploadDir.mkdirs();
-        }
-
-        // 生成完整的文件路径
-        assert fileName != null;
-        File saveFilePath = new File(uploadDir, fileName);
-
-        // 保存文件到磁盘
-        mf.transferTo(saveFilePath);
-
-        if (!fileName.isEmpty()) {
-            // 创建一个 Picture 对象并设置 imgPath
-            Picture picture = new Picture();
-            picture.setImgPath(saveFilePath.getAbsolutePath());
-
-            // 保存图片信息到数据库
-            savePicture(picture);
-        }
-
-        return "saveFilePath:" + saveFilePath;
-    }
-
-    private void savePicture(Picture picture) {
-        pictureS.savePicture(picture);
-    }
 
     @GetMapping(
             value = "/test/getImage",
